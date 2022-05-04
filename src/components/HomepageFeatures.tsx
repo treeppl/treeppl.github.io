d1107b17cedd4a87a12fcc0348feda4244392c87
Daniel Lundén <dlunde@kth.se>
2022-05-04 11:41:21 +0200

    Switch front page to simple text snippet

--- src/components/HomepageFeatures.tsx
+++ src/components/HomepageFeatures.tsx
@@ -2,70 +2,22 @@ import useBaseUrl from '@docusaurus/useBaseUrl';
 import React from 'react';
 import clsx from 'clsx';
 import styles from './HomepageFeatures.module.css';
-
-type FeatureItem = {
-  title: string;
-  image: string;
-  description: JSX.Element;
-};
-
-const FeatureList: FeatureItem[] = [
-  // TODO Replace with TreePPL feature items?
-  {
-    title: 'TODO',
-    image: '/img/todo.png',
-    description: (
-      <>
-        TODO Description
-      </>
-    ),
-  },
-  {
-    title: 'TODO',
-    image: '/img/todo.png',
-    description: (
-      <>
-        TODO Description
-      </>
-    ),
-  },
-  {
-    title: 'TODO',
-    image: '/img/todo.png',
-    description: (
-      <>
-        TODO Description
-      </>
-    ),
-  },
-];
-
-function Feature({title, image, description}: FeatureItem) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <img
-          className={styles.featureSvg}
-          alt={title}
-          src={useBaseUrl(image)}
-        />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
-}
+import Link from '@docusaurus/Link';
 
 export default function HomepageFeatures(): JSX.Element {
   return (
     <section className={styles.features}>
       <div className="container">
         <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
+          <div className={clsx('col')}>
+            <div className="text--center padding-horiz--md">
+              <p>
+                <>
+                  The aim of the TreePPL project is to develop a domain-specific PPL for statistical phylogenetics. More information can be found <Link to="/docs/treeppl">here</Link>.
+                </>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
