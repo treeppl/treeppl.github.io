1400a85b6f0f76fdf08aa50aaaaf9cb0e84f164e
ThimotheeV <Tim.Virgoulay@nrm.se>
2026-01-23 16:46:56 +0100

    Fix merge

--- pull_md.sh
+++ pull_md.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 git clone https://github.com/treeppl/treeppl.git
-=======
-git clone https://github.com/ThimotheeV/treeppl.git
->>>>>>> master
 mv ./treeppl/models/*.md ./docs/model-library
 rm -rf ./treeppl
-
