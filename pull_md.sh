a4aca0005fd8ad059ba7db44cee197b35154a23d
Tim Virgoulay <ThimotheeV@users.noreply.github.com>
2026-01-23 14:46:22 +0100

    Change git clone URL to HTTPS

--- pull_md.sh
+++ pull_md.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone git@github.com:ThimotheeV/treeppl.git
+git clone https://github.com/ThimotheeV/treeppl.git
 mv ./treeppl/models/*.md ./docs/model-library
 rm -rf ./treeppl
 
